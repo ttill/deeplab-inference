2142dd58e8fa0b5a5a6b50223d22dd395c52f41a
Till Theato <till.theato@evocount.de>
2020-06-14 19:30:45 +0200

    Use keras algos for metrics.

--- mt_inference/metrics.py
+++ mt_inference/metrics.py
@@ -3,14 +3,15 @@ from typing import List
 from pathlib import Path
 from PIL import Image
 import numpy as np
+import tensorflow as tf
 
 
 class Metric(ABC):
     def __init__(self, ground_truth: Path, seg_map: np.array):
         image = Image.open(ground_truth)
-        self.ground_truth = np.array(image, dtype=bool)
+        self.ground_truth = np.array(image, dtype=np.uint8)
 
-        self.seg_map = seg_map.astype(bool)
+        self.seg_map = seg_map
 
         self._value = None
 
@@ -32,34 +33,25 @@ class Metric(ABC):
         return str(self.value)
 
 
-class MIoUMetric(Metric):
-    def iou(self, predictions, ground_truth):
-        intersection = np.count_nonzero(predictions * ground_truth)
-        union = np.count_nonzero(predictions + ground_truth)
-
-        if union:
-            return intersection / union
-        else:
-            return 1
+class KerasMetric(Metric):
+    @abstractmethod
+    def _keras_metric(self):
+        pass
 
     def _calculate(self):
-        iou_lupine = self.iou(self.seg_map, self.ground_truth)
-        iou_background = self.iou(~self.seg_map, ~self.ground_truth)
+        metric = self._keras_metric()
+        metric.update_state(self.ground_truth, self.seg_map)
+        return metric.result().numpy()
 
-        return (iou_lupine + iou_background) / 2
+
+class MIoUMetric(KerasMetric):
+    def _keras_metric(self):
+        return tf.keras.metrics.MeanIoU(num_classes=2)
 
 
-class PixelAccuracyMetric(Metric):
-    def _calculate(self):
-        true_positive = np.count_nonzero(self.seg_map * self.ground_truth)
-        true_negative = np.count_nonzero(~self.seg_map * ~self.ground_truth)
-        false_positive_negative = (
-            np.count_nonzero(self.seg_map + self.ground_truth) - true_positive
-        )
-
-        return (true_positive + true_negative) / (
-            true_positive + true_negative + false_positive_negative
-        )
+class PixelAccuracyMetric(KerasMetric):
+    def _keras_metric(self):
+        return tf.keras.metrics.Accuracy()
 
 
 def weighted_metrics_mean(metrics: List[Metric]):
